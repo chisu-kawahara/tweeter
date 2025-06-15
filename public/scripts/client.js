3392cc742619a80edf02fe224a8daabc364b778f
chisu-kawahara <chisu.kawahara@gmail.com>
2025-06-15 12:00:28 -0700

    built a tweet box and put on the web using jQuery

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,39 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweet) {
+  const $tweet = $(`
+    <article class="tweet">
+      <header>
+        <div class="tweet-user">
+          <img src="${tweet.user.avatars}" alt="User avatar">
+          <span class="tweet-name">${tweet.user.name}</span>
+        </div>
+        <span class="tweet-handle">${tweet.user.handle}</span>
+      </header>
+      <div class="tweet-content">
+        <p>${tweet.content.text}</p>
+      </div>
+      <footer>
+        <span class="tweet-timestamp">${tweet.created_at}</span>
+        <div class="tweet-actions">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
+        </div>
+      </footer>
+    </article>
+  `);
+  return $tweet;
+};
+
+const renderTweets = function(tweets) {
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $('#tweets-container').append($tweet);
+  }
+};
+
+// Fake data
+const data = [ /* array of tweet objects */ ];
+renderTweets(data);
